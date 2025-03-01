617953762de170e70c6a040761133a355283d138
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-01 15:18:29 +0530

    1-3-25

---
+++ javascript/lec1/index.js
@@ -0,0 +1,85 @@
+function sum(){
+    return 10 + 10
+}
+
+console.log(sum())
+
+// DOM :- Document Object Model
+
+document.getElementById('demo').innerHTML = "Hello Javascript !!!!!!!!!!!"
+
+/* Javascript Variables */
+
+/* Javascript Statements */
+
+/* Javascript syntax */
+
+// ES5 / ES6
+
+/* What is Variables */
+
+var x = 10
+
+// var is variable decalre keyword
+
+// x is variable name
+
+// = is assignement character
+
+// 10 is value
+
+// var y = 20; var z = 50
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// {
+//     var x = 50
+// }
+
+// function sum() {
+//     var x = 100
+//     return x + x
+// }
+
+// console.log(sum());
+
+
+// console.log(x);
+
+// let variable
+
+// let demo = "toys"
+
+// demo = "truck"
+
+// console.log(demo);
+
+// let demos = "books"
+
+// console.log(demos);
+
+// {
+//     let demo = "laptop"
+//     console.log(demo);
+// }
+
+// console.log(fruit);
+// var fruit = "Mango"
+
+
+// const keyword
+
+// const fruits = "banana"
+// fruits = "mango"
+// console.log(fruits);
+
+
+/* javascript is case sensitive language */
+
+// var demo = "20"
+// var Demo = "50"
+
+// console.log(demo);
+// console.log(Demo);
---
+++ javascript/lec2/index.js
@@ -0,0 +1,122 @@
+/* Javascript Hoisting */
+
+// var keyword
+
+// var x , y , z
+
+// x = 10
+// y = 20
+// z = 30
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// let keyword
+
+// let a , b , c
+
+// a = 40
+// b = 50
+// c = 60
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const keyword
+
+// const i = 70
+
+// console.log(i);
+
+// {
+//     let x
+//     x = 10
+//     x = 20
+// }
+// console.log(x);
+
+/* Javascript string operator */
+
+let str1 = "This is String in Javascript!";
+let str2 = "This is String in Javascript!";
+let str3 = `This
+ is
+  String
+ in
+  Javascript!`;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+/* javascript Template Literals */
+
+let names = "Ramesh"
+
+let profile = `my name is ${names}`
+
+console.log(profile);
+
+/* variable names decalaration */
+
+let profiles = "skillqode"
+
+console.log(profiles);
+
+let camelCase = "camelCase"
+
+console.log(camelCase);
+
+let PascalCase = "PascalCase"
+
+console.log(PascalCase);
+
+let snack_case = "snack_case"
+
+console.log(snack_case);
+
+let $name = "$name"
+
+console.log($name);
+
+let _snack_case = " _snack_case"
+
+console.log(_snack_case);
+
+let  b1 = "1"
+
+console.log(b1);
+
+/* Javascript Operators */
+
+/* Arithmetic Operator */
+
+{
+    let x = 5
+    let y = 5
+    let z = "box"
+
+
+    console.log(x + y);
+    console.log(x - y);
+    console.log(x * y);
+    console.log(x / y);
+    console.log(x % y);
+    console.log(x ** y);
+
+    console.log(x + z);
+    console.log(x - z);
+    console.log(x * z);
+    console.log(x / z);
+    console.log(x ** z);
+    console.log(x % z);
+}
+
+{
+    let str1 = "institute"
+    let str2 = "skillqode"
+
+    console.log(str2 + str1);   
+}
