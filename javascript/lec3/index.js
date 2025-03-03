d328f6b84b2a0d4879c3925a9680a076669a4133
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-03 16:59:34 +0530

    03-03-25

---
+++ javascript/lec3/index.js
@@ -0,0 +1,88 @@
+// // 1-> Enter two numbers input by the user
+// {
+//     var a=prompt("enter 1st number")
+//     var b=prompt("enter 2nd number")
+//     console.log("Addition is :"+a+b )
+// }
+
+// // 2-> calculate the area of rectangle
+
+// {
+// let l=prompt("enter length of rectangle")
+// let b=prompt("enter breath of rectangle")
+
+
+// console.log("Area is :" +l*b)
+// }
+
+// // 3-> Convert temperature from Celsius to Fahrenheit
+
+// {
+//    let t=10
+// console.log("temprature(fahrenheit)="+(t * 9/5 +32));
+
+// }
+
+// // 4-> Find the remainder when one number is divided by another
+
+// {
+//     let n1=prompt("enter 1st number")
+//     let n2=prompt("enter 2nd number")
+//     console.log(n1 % n2);
+// }
+
+// // 5-> Calculate the power of a number
+
+// {
+//     let x=8
+//     let y=2
+//     console.log(x **= y);
+    
+// }
+
+// // 6-> Combination of Arithmetic Operators
+
+// {
+//     let a=1
+//     let b=2
+//     let c=3;d=4;e=5;f=6
+
+//     console.log((a+b)-c+(d*e)/f);
+    
+// }
+
+// 7-> Comparing Numbers [5 number]
+
+// {
+//     var a=prompt("enter 1st number")
+//     var b=prompt("enter 2nd number")
+//     var c=prompt("enter 3rd number")
+//     var d=prompt("enter 4th number")
+//     var e=prompt("enter 5th number")
+//     console.log(a==b);
+//     console.log(c===d);
+//     console.log(e!=a);
+//     console.log(b!==c);
+//     console.log(a>c);
+//     console.log(a>d);
+//     console.log(d>=e);
+//     console.log(c<=e);
+// }
+
+// 8-> Comparing Strings [5 String]
+
+// {
+//     var a=("enter 1st number")
+//     var b=("enter 2nd number")
+//     var c=("enter 3rd number")
+//     var d=("enter 4th number")
+//     var e=("enter 5th number")
+//     console.log(a==b);
+//     console.log(c===d);
+//     console.log(e!=a);
+//     console.log(b!==c);
+//     console.log(a>c);
+//     console.log(a>d);
+//     console.log(d>=e);
+//     console.log(c<=e);
+// }
