59c846ff5920bc9db13f713540af7b69f1180a15
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-04 16:04:15 +0530

    4-3-25

---
+++ javascript/lec4/script.js
@@ -0,0 +1,97 @@
+// Javascript Data Types
+
+/*  Primitive Data Types */
+
+/*
+
+ string
+ number
+ boolean
+ null
+ undefined
+ symbol
+ Bigint
+
+*/
+
+
+/* non-Primitive Data Types */
+
+/*
+
+array
+object
+
+*/
+
+// string data type
+
+
+{
+       let data=10+'10'
+   console.log(data);
+}
+   
+{
+    
+let data=10-'10'
+console.log(data);
+
+}
+
+{
+    let data=10+10-'10'*30
+console.log(data);
+}
+
+// type of operator
+
+{
+    let x = "this is string"
+  
+    console.log(typeof x);
+  
+    let num = 100
+  
+    console.log(typeof num);
+  
+    let value = true
+  
+    console.log(typeof value);
+  
+    let int = 10n
+  
+    console.log(typeof int);
+  
+    let symbol = Symbol()
+  
+    console.log(typeof symbol);
+  
+    let nullValue = null
+  
+    console.log(typeof nullValue);
+  
+    let unde = undefined
+  
+    console.log(typeof unde);
+    
+    let array = []
+  
+    console.log(typeof array);
+  
+    let object = {}
+  
+    console.log(typeof object);
+    
+  }
+
+  /* Javascript alert  , Prompt , and conform */
+
+{
+    alert('This is Javascript !!!!')
+    let age = prompt('age' , )
+    console.log(parseInt(age));
+    let confirms = confirm('Are you sure clear area!!!!')
+    console.log(confirms);
+  }
+
