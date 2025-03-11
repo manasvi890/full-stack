c63751789e4385115549f0dcbae5c6834641598a
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-11 16:17:19 +0530

    11-3-25

---
+++ javascript/lec8/script.js
@@ -0,0 +1,43 @@
+// for in method of loop
+
+
+// {
+//     let object = {}
+ 
+// console.log(object);
+
+// let newObject = new Object()
+
+// console.log(newObject);
+// }
+
+
+{
+    let profile={
+        fullname:"Rajesh",
+    age:20,
+    email:"rajesh123@gmail.com"
+    }
+    for(let key in profile){
+        // console.log(key);
+        // console.log(profile.key);
+        console.log(`${key} = ${profile[key]}`);
+    }
+    
+}
+
+// string methods
+
+
+{
+    
+let str="This is javascript String!"
+console.log(str.length);
+console.log(str.at(10));
+console.log(str.at(-1));
+console.log(str.at(0));
+
+}
+
+
+
