bab6c19b06b0498fa58e27c734ebc48e1e7148b7
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-19 16:29:36 +0530

    19-3

---
+++ javascript/programs/script.js
@@ -0,0 +1,50 @@
+// 1->Javascript program to print hello world
+
+{console.log("Hello World!!!");}
+
+// 2->Javascript program to Add two numbers
+
+{let num1 = (45)
+let num2 = (50)
+console.log("Addition :",num1 + num2);}
+
+// 3->JavaScript Program to Find the Square Root
+
+{let a = Math.sqrt(25)
+console.log("Square root:",a);}
+
+// 4->JavaScript Program to Calculate the Area of a Triangle
+
+{let base = (10)
+let height = (5)
+console.log("Area of triangle:", base*height/2);}
+
+// 5->JavaScript Program to Swap Two Variables
+
+{
+    console.log('swap variables:');
+    
+let a = 40;
+let b = 30;
+
+console.log(`      before swap a=${a} b=${b}`);
+[b,a]=[a,b]
+console.log(`      After swap a=${a} b=${b}`);
+
+}
+
+// 6->JavaScript Program to Solve Quadratic Equation
+console.log("Quadratic Equation:");
+
+
+// 7->JavaScript Program to Convert Kilometres to Miles
+
+let km = (2)
+console.log("km to miles:",0.62137*km);
+
+// 8->Javascript Program to Convert Celsius to Fahrenheit
+
+let c = (45)
+console.log("Fahrenheit:",(c*9/5)+32);
+
+// 9->Javascript Program to Generate a Random Number
