e2b7e2b548c1724c9c32fa16c1f7af42cf06fc46
Hitesh Bhoot <hiteshbhut666@gmail.com>
2025-03-19 15:55:52 +0530

    19-3-25

---
+++ javascript/lec12 Math/script.js
@@ -0,0 +1,120 @@
+// Math Function
+
+// math.round
+
+{
+let math1 = Math.round(45.89)
+console.log(math1);
+
+let math2 = Math.round(45.10)
+console.log(math2);
+}
+
+// math.PI
+
+{
+    let math1 = Math.PI
+    console.log(math1);
+}
+
+// Math.abs
+
+{
+    let math2 = Math.abs(-10)
+
+    console.log(math2);
+
+    let math3 = Math.abs(-0)
+
+    console.log(math3);
+}
+
+// Math.ceil
+
+{
+    let math4 = Math.ceil(45.1)
+
+    console.log(math4);
+}
+
+// Math.floor
+
+{
+    let math5 = Math.floor(45.9)
+
+    console.log(math5);
+}
+
+// Math.sign
+
+{
+    let math6 = Math.sign(-45)
+
+    console.log(math6);
+}
+
+// Math.sin
+
+{
+    let math7 = Math.sin(90*3.14/180)
+
+    console.log(math7);
+
+    let math8 = Math.sin(30*3.14/180)
+
+    console.log(math8);
+}
+
+// Math.round
+
+{
+    let math9=  Math.round(45.5)
+
+    console.log(math9);
+}
+
+// Math.cos
+
+{
+    let math10 = Math.cos(90*3.14/180)
+
+    console.log(math10);
+}
+
+// Math.max
+
+{
+    let math11 = Math.max(100 , 201 , 30 , 40 , 45 , 78)
+
+    console.log(math11);
+}
+
+// Math.min
+
+{
+    let math12 = Math.min(100 , 201 , 30 , 40 , 45 , 78)
+
+    console.log(math12);
+}
+
+// Math.trunc
+
+{
+    let math13 = Math.trunc(-45.53)
+
+    console.log(math13);
+}
+
+// Math.log
+// Math.log2
+// Math.log10
+
+{
+    let math14 = Math.log(10)
+    let math15 = Math.log2(10)
+    let math16 = Math.log10(10)
+
+    console.log(math14);
+    console.log(math15);
+    console.log(math16);
+}
